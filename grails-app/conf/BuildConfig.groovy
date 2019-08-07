7749190898d046f00e29d0051b83e6511f4298ae
nessim <nessim.kisserli@gmail.com>
2019-08-07 15:23:41 +0200

    Make jetty the development container for grails

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -59,6 +59,8 @@ grails.project.dependency.resolution = {
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
         compile ":asset-pipeline:1.9.9"
+        compile "org.grails.plugins:jetty:3.0.0" // required to run the app.
+
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
